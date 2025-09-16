42a880e8b525bbc432e5906013be9142cf99eadb
shorifulbd1st <shorifulbd1st@gmail.com>
2025-09-16 10:52:11 +0600

    ensure pages start from top after navigation

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 const Main = () => {
-    return (
-        <div >
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar></Navbar>
+      <ScrollToTop></ScrollToTop>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
